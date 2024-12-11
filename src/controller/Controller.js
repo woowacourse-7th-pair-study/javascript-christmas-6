e52c5e27a2fdc8f7173951ccfb7436726870304e
ohgus <123801385+ohgus@users.noreply.github.com>
2024-12-11 19:08:35 +0900

    feat: controller class 구현
    - input 입력 관련 기능 구현

---
+++ src/controller/Controller.js
@@ -0,0 +1,53 @@
+import InputView from '../view/InputView.js';
+import Validate from '../validate/Validate.js';
+import Menu from '../model/Menu.js';
+import EventCalendar from '../model/EventCalendar.js';
+import Order from '../model/Order.js';
+import tryCatch from '../util/tryCatch.js';
+
+import { MENU } from '../constants/menu.js';
+
+class Controller {
+  #menu;
+
+  constructor() {
+    this.#menu = new Menu(MENU);
+  }
+  async play() {
+    const date = await tryCatch(() => this.#readDate());
+    const orders = await tryCatch(() => this.#readOrder());
+    const ordersInstance = this.#generateOrderInstance(orders);
+  }
+
+  async #readDate() {
+    const date = await InputView.readDate();
+    Validate.date(Number(date));
+
+    return date;
+  }
+
+  async #readOrder() {
+    const orderInput = await InputView.readOrder();
+    const orders = orderInput
+      .split(',')
+      .map((order) => order.split('-'))
+      .map(([name, count]) => [name, Number(count)]);
+    Validate.order(orders, this.#menu);
+
+    return orders;
+  }
+
+  #generateOrderInstance(orders) {
+    return orders.map(([name, count]) => {
+      const category = this.#menu.getCategory(name);
+      const price = this.#menu.getPrice(name);
+
+      return new Order({ name, count, category, price });
+    });
+  }
+}
+
+export default Controller;
+
+const controller = new Controller();
+controller.play();
