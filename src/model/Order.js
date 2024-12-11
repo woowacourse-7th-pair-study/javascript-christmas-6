815a880012e0a68ca73d92d644a5a6a50c9bd38e
ohgus <123801385+ohgus@users.noreply.github.com>
2024-12-11 19:06:28 +0900

    refactor: Order class 구조 변경

--- src/model/Order.js
+++ src/model/Order.js
@@ -1,12 +1,14 @@
-import { MENU } from "../constants/menu";
 export default class Order {
   // [양송이스프,1]
   #name;
   #count;
   #category;
-  constructor([name, count], category) {
+  #price;
+
+  constructor({ name, count, category, price }) {
     this.#name = name;
     this.#count = count;
     this.#category = category;
+    this.#price = price;
   }
 }
