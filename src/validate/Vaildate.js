fecefca932f7c9cbaf9683c88ec6194f0f1981c2
Ko Daeun <drechsu@gmail.com>
2024-12-11 17:29:33 +0900

    feat(Validate) : 메뉴 검증하는 로직 구현

--- src/validate/Vaildate.js
+++ src/validate/Vaildate.js
@@ -6,7 +6,38 @@ export default class Validate {
     this.#isInRange(date);
     this.#isInteger(date);
   }
-
+  //[[양송이스프,1],[초코케이크,2]]
+  static order(orderArr) {
+    let totalCount = 0;
+    const nameArr = [];
+    orderArr.forEach(([name, count]) => {
+      totalCount += count;
+      this.#isValidCount(totalCount);
+      this.#isDuplicate(name, nameArr);
+      nameArr.push(name);
+      this.#validateCount(count);
+    });
+  }
+  static #validateCount(count) {
+    this.#isNumber(count);
+    this.#isInteger(count);
+    this.#isOVerOne(count);
+  }
+  static #isOVerOne(count) {
+    if (count < 1) {
+      throw new Error(ERROR_MESSAGES.order);
+    }
+  }
+  static #isValidCount(totalCount) {
+    if (totalCount > 20) {
+      throw new Error(ERROR_MESSAGES.order);
+    }
+  }
+  static #isDuplicate(name, nameArr) {
+    if (nameArr.includes(name)) {
+      throw new Error(ERROR_MESSAGES.order);
+    }
+  }
   static #isNumber(date) {
     if (Number.isNaN(date)) {
       throw new Error(ERROR_MESSAGES.date);
