0680a7b26118ea9656ad5d126fcc61f667dcf149
ohgus <123801385+ohgus@users.noreply.github.com>
2024-12-11 19:07:32 +0900

    refactor: 파일 불러오기 오타 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,5 @@
-import { Console } from "@woowacourse/mission-utils";
-import { INPUT_MESSAGES } from "../constants/messages";
+import { Console } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGES } from '../constants/messages.js';
 const InputView = {
   async readDate() {
     const input = await Console.readLineAsync(INPUT_MESSAGES.date);
